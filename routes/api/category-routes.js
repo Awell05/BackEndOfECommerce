9f200a1f6d817fd7fec3fb1d26f599d5b463bf5a
Novo Persona <adcaldwell93@yahoo.com>
2022-11-03 09:41:17 -0400

    completed category-routes.js in routes folder

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,8 +34,11 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
 try {
-  const createCategory = await Category.create(req.body)
-
+  const createCategory = await Category.create(req.body);
+  
+res.status(200).json(createCategory);
+} catch (err) {
+  res.status(500).json(err);
 }
 });
 
